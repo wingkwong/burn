a9e4bbf13cea928809b4f33fcfbe0db2afef445a
wingkwong <wingkwong.code@gmail.com>
2018-09-11 14:41:19 +0800

    added Apollo config

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,67 @@
 import Vue from 'vue'
+
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { persistCache } from 'apollo-cache-persist'
+import { onError } from "apollo-link-error"
+import { ApolloLink, split } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import { enableExperimentalFragmentVariables } from 'graphql-tag'
+import VueApollo from 'vue-apollo'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/style.scss'
+
+import ElementUI from 'element-ui'
+
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+const uri = `${process.env.VUE_APP_URI}/graphql`
+const httpLink = new HttpLink({
+  uri,
+})
+
+const cache = new InMemoryCache()
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    )
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+})
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    errorLink,
+    httpLink
+  ]),
+  cache,
+  connectToDevTools: true,
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: client,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
+Vue.use(VueApollo)
 
 new Vue({
   router,
+  provide: apolloProvider.provide(),
   store,
   render: h => h(App)
 }).$mount('#app')
