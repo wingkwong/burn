bd8e93d4eb2de3b8abca965f6bfecb606bad36e2
wingkwong <wingkwong.code@gmail.com>
2018-09-25 15:35:28 +0800

    get message after clicking open btn

--- client/src/views/Message.vue
+++ client/src/views/Message.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="message">
-    <div v-if="!submitted">
-	   You are about to open message {{$route.params.id}}
-	 </div>
 
-	  <div v-if="submitted">
-	  	{{this.content}}
-	  </div>
+	   
+     <div v-if="!submitted">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>You are about to open message {{$route.params.id}}</span>
+          </div>
+          <el-button type="primary" @click="openMessage">Open</el-button>
+          <el-button type="danger" @click="deleteMessage">Delete</el-button>
+        </el-card>
+     </div>
+     <div v-if="submitted">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>This message will be deleted after closing the window.</span>
+        </div>
+      {{this.content}}
+      </el-card>
+      </div>
   </div>
 </template>
 
@@ -22,19 +34,29 @@ export default {
 	    content: ''
     }
   },
-   apollo: {
-    getMessage: {
-      query: GetMessage,
-      variables() {
-        return {id: this.$route.params.id}
-      },
-      pollInterval: 90000,
-      result ({data: { getMessage }}) {
+  methods: {
+    openMessage: function() {
+      this.$apollo.query({
+        query: GetMessage,
+        variables:{
+          id: this.$route.params.id
+        }
+      }).then(({data: { getMessage }}) => {
        this.submitted = true
        this.content = getMessage.message
-       console.log(getMessage)
-      },
-    }
+       this.error = false
+      }).catch((error) => {
+        if (error.graphQLErrors.length >= 1) {
+          this.error = error.graphQLErrors[0].message            
+        } else {
+          this.error = 'Something went wrong'
+        }
+        console.log(error)
+      })
+    },
+    deleteMessage: function() {
+      alert('delete')
+    },
   }
 }
 
